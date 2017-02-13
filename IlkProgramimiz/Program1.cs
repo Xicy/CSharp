77e8a2d05f31398db328cc275305ad780fab7fa4
Umut Akkaya <umutakkaya1996@gmail.com>
2017-02-13 20:19:51 +0300

    Ilk Programimiz

---
+++ IlkProgramimiz/Program1.cs
@@ -0,0 +1,12 @@
+﻿namespace IlkProgramimiz
+{
+    class Program1
+    {
+        // .NET çalışma zamanında ön tanımlı olarak Main() fonksiyonunu çalıştırır.
+        static void Main()
+        {
+            // Console sınıfı içerisindeki Writeline() fonksiyonu çalıştırılır
+            System.Console.WriteLine("Merhaba Dünya");
+        }
+    }
+}
