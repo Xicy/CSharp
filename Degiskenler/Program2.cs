964237e0d1e5cf39d02d37d9fbc3f660dbd12720
Umut Akkaya <umutakkaya1996@gmail.com>
2017-02-13 20:20:03 +0300

    Degiskenler

---
+++ Degiskenler/Program2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Degiskenler
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            /*Konsoldan metin alıp ekrana
+             * .... metnini yazdınız 
+             * şeklinde bastırma 
+             */
+        }
+    }
+}
